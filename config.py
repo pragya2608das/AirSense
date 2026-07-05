309092e6761b01ffa61272e8d91354e9848c6558
Pragya Karnean <k.pragya2006@gmail.com>
2026-07-05 08:01:20 +0530

    Prepared project for Streamlit deployment

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
-load_dotenv(override=True)
+load_dotenv()
 
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+# Streamlit Cloud fallback
+if OPENWEATHER_API_KEY is None:
+    try:
+        import streamlit as st
+        OPENWEATHER_API_KEY = st.secrets["OPENWEATHER_API_KEY"]
+    except Exception:
+        OPENWEATHER_API_KEY = None
